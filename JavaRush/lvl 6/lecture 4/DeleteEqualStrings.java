60c85514d7512aa96d0c8df96731b478fec1ef50
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-09 13:49:01 +0300

    Update DeleteEqualStrings.java

--- JavaRush/lvl 6/lecture 4/DeleteEqualStrings.java
+++ JavaRush/lvl 6/lecture 4/DeleteEqualStrings.java
@@ -7,16 +7,16 @@ public class DeleteEqualStrings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arrSize = 10;
-        
+
         strings = new String[arrSize];
 
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < arrSize; i++) {
             strings[i] = sc.nextLine();
         }
 
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < arrSize; i++) {
             String stringsCopy = strings[i];
-            for (int j = i + 1; j < strings.length; j++) {
+            for (int j = i + 1; j < arrSize; j++) {
                 if (stringsCopy == null) {
                     break;
                 }
@@ -30,7 +30,7 @@ public class DeleteEqualStrings {
         /* fancy way to print array
         Arrays.stream(strings).forEach(System.out::println);
 */
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < arrSize; i++) {
             System.out.println(strings[i]);
         }
     }
