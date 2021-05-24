b872fd8b51946a0fe11d3a5a48bc57240ec91d60
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-24 20:39:06 +0300

    Add files via upload

---
+++ JavaRush/lvl 7/lecture_6/PiGeometry.java
@@ -0,0 +1,13 @@
+// simple problem from JavaRush
+// calculate a perimeter and square of a circle
+// use final variable Pi
+public class PiGeometry {
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("square of a circle is : " + pi * radius * radius);
+        System.out.println("perimeter of a circle is : " + pi * diameter);
+    }
+}
