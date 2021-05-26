3b00d0a6a1cd7b30da3abc519eeb19998e5efcae
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-26 21:05:18 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_1/AllClasses.java
@@ -0,0 +1,14 @@
+// primitive JavaRush problem
+// declare all data types variables
+public class AllClasses {
+    public static void main(String[] args) {
+        byte byTe;
+        short shorTy;
+        int inTeger;
+        long lonGest;
+        float fLoat;
+        double dOuble;
+        boolean booLean;
+        char cHar;
+    }
+}
