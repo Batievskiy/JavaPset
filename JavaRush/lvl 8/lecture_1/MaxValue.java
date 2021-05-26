cb5f11f36a6274c9842cce45a67f3f901e7dd578
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-26 21:05:47 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_1/MaxValue.java
@@ -0,0 +1,10 @@
+// primitive JavaRush problem
+// initialize variables with it max values
+public class MaxValue {
+    public static void main(String[] args) {
+        byte bYte = Byte.MAX_VALUE;
+        int iNt = Integer.MAX_VALUE;
+        long loNg = Long.MAX_VALUE;
+        short sHort = Short.MAX_VALUE;
+    }
+}
