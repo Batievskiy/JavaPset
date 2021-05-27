dcfd61b34b5b5d7f0c6766e420b060d5fd366f48
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-27 12:11:41 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_1/MinValue.java
@@ -0,0 +1,16 @@
+// primitive problem from JavaRush
+// initialize all variable types with min value
+public class MinValue {
+
+    public static void main(String[] args) {
+
+        // -128
+        byte bYte = Byte.MIN_VALUE;
+        // -32768
+        short sHort = Short.MIN_VALUE;
+        // -2147483648
+        int iNt = Integer.MIN_VALUE;
+        // -9223372036854775808
+        long loNg = Long.MIN_VALUE;
+    }
+}
