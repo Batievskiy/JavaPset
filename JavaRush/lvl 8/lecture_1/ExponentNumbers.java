4448a4a4fc084e46bf43fc3d069d345f40c92f02
Batievskiy <batievskiy.e.v@gmail.com>
2021-05-29 17:22:59 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_1/ExponentNumbers.java
@@ -0,0 +1,21 @@
+// Primitive problem from JavaRush
+// write all variables using exponent
+public class ExponentNumbers {
+    public static void main(String[] args) {
+
+        /*
+        double earthDiameter = 12742.0;
+        double lightSpeed = 299792458.0;
+        double uraniumAtomicMass = 238.0289;
+        double averageBeeWeight = 0.085;
+        double javaDeveloperSalary = 10000.0;
+         */
+
+        double earthDiameter = 1.2742E+4;
+        double lightSpeed = 2.99792458E+8;
+        double uraniumAtomicMass = 2.380289E+2;
+        double averageBeeWeight = 8.5E-2;
+        double javaDeveloperSalary = 1.E+4;
+
+    }
+}
