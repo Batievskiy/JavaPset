92d2791775cf5c1faa190c5ac2d5c0b3c791f9d0
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-05 20:56:17 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_4/FirstObjects.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+// dumb problem from JavaRush - lvl 8 / lecture 4
+// create lots of Objects
+public class FirstObjects {
+    public static void main(String[] args) {
+        String s1 = new String();
+        String s2 = new String();
+        String s3 = new String();
+        String s4 = new String();
+        String s5 = new String();
+        String s6 = new String();
+        String s7 = new String();
+        String s8 = new String();
+        String s9 = new String();
+        String s10 = new String();
+        int [] a1 = new int[0];
+        int [] a2 = new int[0];
+        int [] a3 = new int[0];
+        int [] a4 = new int[0];
+        int [] a5 = new int[0];
+        Scanner scanner1 = new Scanner("");
+        Scanner scanner2 = new Scanner("");
+    }
+}
