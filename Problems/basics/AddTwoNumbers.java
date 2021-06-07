22d5e6daa8beabfdf1ab125bf491611bebe80a6b
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-07 18:10:57 +0300

    Add files via upload

---
+++ Problems/basics/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+// specify the value of both the numbers in the program itself
+// and add two numbers
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int number1 = 11, number2 = 22, sum;
+        sum = number1 + number2;
+
+        System.out.println("sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
