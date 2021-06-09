16e2d3bd6af726c5117e03bc8b50bb2e4ac1caa8
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-09 13:01:16 +0300

    Add files via upload

---
+++ JavaRush/lvl 8/lecture_7/TerranHumanWoman.java
@@ -0,0 +1,16 @@
+// primitive problem from JavaRush
+// inherit class Woman from class Human from super class Terran
+public class TerranHumanWoman {
+    // Super Class
+    private static class Terran {
+
+    }
+    // first subclass
+    private static class Human extends Terran {
+
+    }
+    // second subclass
+    private static class Woman extends Human {
+
+    }
+}
