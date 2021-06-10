4f461337d8bf343b217f241467ccb87c62fe54b6
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-10 19:31:13 +0300

    Update TrigonometricMethods.java

--- JavaRush/lvl 9/lecture_1/TrigonometricMethods.java
+++ JavaRush/lvl 9/lecture_1/TrigonometricMethods.java
@@ -21,26 +21,26 @@ public class TrigonometricMethods {
         System.out.println("\n---< let's calculate sin, cos and tan of angle: ");
         // let's use Math. methods in Java
         System.out.println("sine of " + degree + " is: " + Math.sin(Math.toRadians(degree)));
-        System.out.println("cosine of " + degree + " is: " + Math.cos(Math.toRadians(degree)));
-        System.out.println("tan of " + degree + " is: " + Math.tan(Math.toRadians(degree)));
-
-        System.out.println("\n---< let's calculate sinh, cosh and tanh of angle: ");
-        System.out.println("hyperbolic sine of " + degree + " is: " + Math.sinh(Math.toRadians(degree)));
-        System.out.println("hyperbolic cosin of " + degree + " is: " + Math.cosh(Math.toRadians(degree)));
-        System.out.println("hyperbolic tan of " + degree + " is: " + Math.tanh(Math.toRadians(degree)));
-
-        System.out.println("\n---< let's calculate asin, acos and atan of angle: ");
-        System.out.println("arc sine of " + degree + " is: " + Math.asin(Math.toRadians(degree)));
-        System.out.println("arc cosine of " + degree + " is: " + Math.acos(Math.toRadians(degree)));
-        System.out.println("arc tan of " + degree + " is: " + Math.atan(Math.toRadians(degree)));
-
-        System.out.println("\n---< let's calculate asin, acos and atan of angle: ");
-        double secOfAngle = 1 / Math.sin(Math.toRadians(degree));
-        double cosecOfAngle = 1 / Math.cos(Math.toRadians(degree));
-        double cotOfAngle = 1 / Math.tan(Math.toRadians(degree));
-        System.out.println("Sec of " + degree + " is: " + secOfAngle);
-        System.out.println("Cosec of " + degree + " is: " + cosecOfAngle);
-        System.out.println("Cotangent of " + degree + " is: " + cotOfAngle);
+//        System.out.println("cosine of " + degree + " is: " + Math.cos(Math.toRadians(degree)));
+//        System.out.println("tan of " + degree + " is: " + Math.tan(Math.toRadians(degree)));
+//
+//        System.out.println("\n---< let's calculate sinh, cosh and tanh of angle: ");
+//        System.out.println("hyperbolic sine of " + degree + " is: " + Math.sinh(Math.toRadians(degree)));
+//        System.out.println("hyperbolic cosin of " + degree + " is: " + Math.cosh(Math.toRadians(degree)));
+//        System.out.println("hyperbolic tan of " + degree + " is: " + Math.tanh(Math.toRadians(degree)));
+//
+//        System.out.println("\n---< let's calculate asin, acos and atan of angle: ");
+//        System.out.println("arc sine of " + degree + " is: " + Math.asin(Math.toRadians(degree)));
+//        System.out.println("arc cosine of " + degree + " is: " + Math.acos(Math.toRadians(degree)));
+//        System.out.println("arc tan of " + degree + " is: " + Math.atan(Math.toRadians(degree)));
+//
+//        System.out.println("\n---< let's calculate asin, acos and atan of angle: ");
+//        double secOfAngle = 1 / Math.sin(Math.toRadians(degree));
+//        double cosecOfAngle = 1 / Math.cos(Math.toRadians(degree));
+//        double cotOfAngle = 1 / Math.tan(Math.toRadians(degree));
+//        System.out.println("Sec of " + degree + " is: " + secOfAngle);
+//        System.out.println("Cosec of " + degree + " is: " + cosecOfAngle);
+//        System.out.println("Cotangent of " + degree + " is: " + cotOfAngle);
     }
 
     private static double getAngle() {
@@ -48,8 +48,8 @@ public class TrigonometricMethods {
         do {
             System.out.print("enter angle value in degree: ");
             angle = sc.nextLine().trim();
-            // not bad Regular Expression for double - "^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$"
-            if (!angle.matches("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$")) {
+            // Regular Expression for degrees from 0 to 360 - "(-?)([1-2]?[0-9]?[0-9]|3[0-5][0-9]|360)"
+            if (!angle.matches("(-?)([1-2]?[0-9]?[0-9]|3[0-5][0-9]|360)")) {
                 System.out.println("this is not degree! Try again!");
                 angle = "";
             }
