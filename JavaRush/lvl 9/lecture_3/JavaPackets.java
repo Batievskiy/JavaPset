9b84e618ef9c943d3b4e6c142cb3685daf0d1e3a
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-11 16:16:11 +0300

    Add files via upload

---
+++ JavaRush/lvl 9/lecture_3/JavaPackets.java
@@ -0,0 +1,19 @@
+// what is packets in Java?
+// let's find out
+
+// package is a folder where particular file is located:
+// folder - c:\projects\data\project\src\games\best\levels
+// package - package games.best.levels;
+
+// Classes import
+// import package.full_unique_classname
+// examples:
+import java.util.Scanner;
+import java.lang.String;
+// all Classes from java.lang. is automatically imported
+// - we don't have to write this actually :)
+import java.io.FileInputStream;
+import java.util.*; // we can use all Classes from java.util. packet
+
+public class JavaPackets {
+}
