6c75abfa53d04f02d7298b42c2fa1fafbd453765
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-22 10:12:44 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_1/BooleanLiterals.java
@@ -0,0 +1,23 @@
+// what is the Boolean Literals in Java?
+// let's find out
+public class BooleanLiterals {
+    public static void main(String[] args) {
+        System.out.println("---< Boolean Literals in Java >---");
+        // Literal - > any value
+        // which can be assigned to the variable
+        // is called as literal/constant
+
+        // Only two values are allowed for Boolean literals
+        // i.e. True and False
+
+        boolean isA = true;
+        boolean isB = false;
+
+        System.out.println("True or False literals"
+                + "\nboolean isA = true;"
+                + "\n---< " + isA);
+        System.out.println("True or False literals"
+                + "\nboolean isB = false;"
+                + "\n---< " + isB);
+    }
+}
