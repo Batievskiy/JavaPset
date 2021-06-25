22bb7686371d72f16e3ee1d1ec6bd68b0958f4da
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-25 14:12:35 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_1/Literals.java
@@ -0,0 +1,18 @@
+// primitive JavaRush problem
+// change literals for correct compilation
+public class Literals {
+
+    /* Source code to change:
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
+     */
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
+    public static void main(String[] args) {
+
+    }
+}
