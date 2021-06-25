7b9285b8a43226b0d4b47408ac8fe39c192ad022
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-25 14:16:31 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_1/FloatDotLiteral.java
@@ -0,0 +1,21 @@
+// primitive JavaRush problem
+// change literals for code compile
+public class FloatDotLiteral {
+
+    /* Source code to change:
+    public int a = 0.;
+    public int b = .0;
+    public int c = 100D;
+    public int d = 100.0;
+    public int e = 1.11E5;
+    public int f = 200F;
+    public int g = 0.F;
+     */
+    public double a = 0.;
+    public double b = .0;
+    public double c = 100D;
+    public double d = 100.0;
+    public double e = 1.11E5;
+    public float f = 200F;
+    public float g = 0.F;
+}
