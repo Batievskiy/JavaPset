99a245e2f7fadc2c08fe7be4bb69be1c1a38e3c4
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-26 06:55:27 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_1/ConcatenationStrings.java
@@ -0,0 +1,10 @@
+// JavaRush primitive problem
+// Split string on smaller pieces
+// and concatenate them.
+public class ConcatenationStrings {
+    public String text = "Всегда пишите код так, " +
+            "будто сопровождать его будет " +
+            "склонный к насилию психопат, " +
+            "который знает, где вы живете. " +
+            "Martin Golding";
+}
