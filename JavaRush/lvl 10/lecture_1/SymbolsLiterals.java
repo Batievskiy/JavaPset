7caa80d9d74ad121ff823c26106734efb4e32b86
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-30 12:01:35 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_1/SymbolsLiterals.java
@@ -0,0 +1,19 @@
+// JavaRush problem
+// change:
+// first to \u004A
+// second to 'a'
+// third to \u0076
+// fourth to 'a'
+public class SymbolsLiterals {
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
+
+    public static void main(String[] args) {
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
+        System.out.print(fourth);
+    }
+}
