c487df3e0c5568650a75476875f98249a27105bd
Batievskiy <batievskiy.e.v@gmail.com>
2021-06-30 11:53:05 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_3/EscapeSymbolsJavaRush.java
@@ -0,0 +1,8 @@
+// JavaRush primitive problem
+// use escape symbols to print our paths
+public class EscapeSymbolsJavaRush {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
