3304f374fb70df489072eaa9867c8d49262052a5
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-02 09:05:58 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_3/ConstantsJavaRush.java
@@ -0,0 +1,10 @@
+// JavaRush lvl 11 lecture 2
+// what is constants in Java?
+public class ConstantsJavaRush {
+    public final int maxAge = 25;
+    public final int maxWeight;
+
+    public ConstantsJavaRush(int weight) {
+        this.maxWeight = weight; // starting value in constant
+    }
+}
