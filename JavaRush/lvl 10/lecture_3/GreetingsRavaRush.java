4c0cfb997d0542d0a17b3ff6ce190dad36840616
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-02 09:00:40 +0300

    Add files via upload

---
+++ JavaRush/lvl 10/lecture_3/GreetingsRavaRush.java
@@ -0,0 +1,19 @@
+// lvl 10 lecture 3
+// print out greetings using unicode
+public class GreetingsRavaRush {
+    public static String partyFace = "\uD83E\uDD73";
+    public static String balloon = "\uD83C\uDF88";
+    public static String gift = "\uD83C\uDF81";
+    public static String partyPopper = "\uD83C\uDF89";
+    public static String cake = "\uD83C\uDF82";
+
+    public static void main(String[] args) {
+        printCongratulation();
+    }
+
+    public static void printCongratulation() {
+        String happyBirthday = "С днем рождения!";
+        String congratulation = partyFace + balloon + partyPopper + cake + gift;
+        System.out.println(happyBirthday + congratulation);
+    }
+}
