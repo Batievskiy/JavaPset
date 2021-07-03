a9cd8c870b109c7758e022060ef487a46493c76b
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-03 14:17:04 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_7/AbstractionJavaRush.java
@@ -0,0 +1,19 @@
+// JavaRush lvl 11 lecture 7
+// Abstraction basic principle
+public class AbstractionJavaRush {
+    // Abstraction basic principle - Remove all unnecessary values from Class
+}
+class Employee {
+    // all necessary values fo Employee is:
+    String name;
+    int age;
+    int socialInsuranceNumber;
+    int taxNumber;
+}
+// but for TopModel
+class TopModel {
+    String name;
+    int height;
+    String hairColor;
+    String eyeColor;
+}
