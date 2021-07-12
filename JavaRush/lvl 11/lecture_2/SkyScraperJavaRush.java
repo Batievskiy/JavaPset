2b8f78a0399ea68dacaf52087205dd5074e83191
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-12 09:05:37 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_2/SkyScraperJavaRush.java
@@ -0,0 +1,11 @@
+// JavaRush primitive problem
+// lvl 11 lecture 2
+// create Skyscraper
+public class SkyScraperJavaRush {
+    public SkyScraperJavaRush() {
+        System.out.println("SkyScrapper has been built");
+    }
+    public static void main(String[] args) {
+        SkyScraperJavaRush skyScraper = new SkyScraperJavaRush();
+    }
+}
