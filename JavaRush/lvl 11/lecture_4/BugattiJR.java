e9b39146bbd4bd890827f6033f55f71c8cb3f01a
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-15 13:37:48 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_4/BugattiJR.java
@@ -0,0 +1,19 @@
+/* JavaRush problem lvl 11 lecture 4
+   Class Bugatti JR must contain:
+   - three fields String color, int year, String body
+   - public getter getBody
+   - public setter setBody(String)
+*/
+public class BugattiJR {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
