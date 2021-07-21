3232ccba0b72d0e8e1786d96a4bcbd1a8d2a0de6
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-21 17:52:28 +0300

    Add files via upload

---
+++ JavaRush/lvl 12/lecture_6/Coffee.java
@@ -0,0 +1,10 @@
+// JavaRush lvl 12 lecture 6 problem
+// correct program to prepare a coffee
+public class Coffee {
+    public static class Milk {
+
+    }
+}
+/* public */ class Sugar { // <--- Delete public status
+
+}
