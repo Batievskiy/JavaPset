daf9370b6d8acba928c4686edebbe847ae372094
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-22 13:14:53 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_6/HotelJR.java
@@ -0,0 +1,15 @@
+// JavaRush lvl 11 lecture 6 problem
+// correct program
+public class HotelJR {
+    /* correct the initialization down below:
+    public int fourthFloor = thirdFloor + 1;
+    public int sixthFloor = fifthFloor + 1;
+    public int thirdFloor = 3;
+    public int fifthFloor = fourthFloor + 1;
+     */
+
+    public int thirdFloor = 3;
+    public int fourthFloor = thirdFloor + 1;
+    public int fifthFloor = fourthFloor + 1;
+    public int sixthFloor = fifthFloor + 1;
+}
