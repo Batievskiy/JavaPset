2e68661d3624579315446495bfbca397192ceff8
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-23 19:23:51 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_6/ComputerJR.java
@@ -0,0 +1,24 @@
+// JavaRush lvl 11 lecture 6
+// create computer
+public class ComputerJR {
+    Keyboard keyboard = new Keyboard();
+    SystemUnit systemUnit = new SystemUnit();
+    Mouse mouse = new Mouse();
+    Monitor monitor = new Monitor();
+}
+
+class Keyboard {
+
+}
+
+class SystemUnit {
+
+}
+
+class Mouse {
+
+}
+
+class Monitor {
+
+}
