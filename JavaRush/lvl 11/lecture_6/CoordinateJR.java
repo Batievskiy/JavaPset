e5a139e565be7ce602fc651e90ac3ed5cd107411
Batievskiy <batievskiy.e.v@gmail.com>
2021-07-23 19:24:04 +0300

    Add files via upload

---
+++ JavaRush/lvl 11/lecture_6/CoordinateJR.java
@@ -0,0 +1,42 @@
+// JavaRush lvl 11 lecture 6 problem
+// correct program
+public class CoordinateJR {
+    /* correct code below:
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        x = this.x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        y = this.y;
+    }
+     */
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
