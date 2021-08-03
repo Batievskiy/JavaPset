59424073d7e795939b47a16142e64cf8333130fb
Batievskiy <batievskiy.e.v@gmail.com>
2021-08-03 10:16:11 +0300

    Add files via upload

---
+++ JavaRush/lvl 13/lecture_6/SolutionLvl13L6v4.java
@@ -0,0 +1,24 @@
+// JavaRush lvl 13 lecture 6 problem
+// change type of variables to diamond(<>) operator
+
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+
+public class SolutionLvl13L6v4 {
+    public static void main(String[] args) {
+//        ArrayList<String> stringArrayList = new ArrayList<String>();
+//        Stack<String> stack = new Stack<String>();
+//        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+//        var exceptionsList = new ArrayList<Exception>();
+//        var fileStack = new Stack<File>();
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
