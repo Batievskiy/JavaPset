f7162279d25eaab0d1a3848aca4d3a14b6f55a89
Batievskiy <batievskiy.e.v@gmail.com>
2021-08-03 10:15:33 +0300

    Add files via upload

---
+++ JavaRush/lvl 13/lecture_6/SolutionLvl13L6v2.java
@@ -0,0 +1,37 @@
+// JavaRush lvl 13 lecture 6 problem
+// change all var to proper types
+// correct getters and setters
+public class SolutionLvl13L6v2 {
+//    private var intArray = new int[14];
+//
+//    public var getIntArray() {
+//        return intArray;
+//    }
+//
+//    public void setArray(var intArray) {
+//        this.intArray = intArray;
+//    }
+//
+//    public static void main(String[] args) {
+//        var solution = new SolutionLvl13L6v2();
+//        var string = new String("Hello");
+//        var stringBuilder = new StringBuilder();
+//    }
+
+    private int[] intArray = new int[14];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        SolutionLvl13L6v2 solution = new SolutionLvl13L6v2();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
