7db068302f62860f388a8e2da5d2aca5419e880e
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-12 11:21:27 +0300

    Add files via upload

---
+++ JavaRush/lvl 18/lecture_7/SolutionLvl18L7v1.java
@@ -0,0 +1,30 @@
+// implement the interface Eatable only for classes,
+// which objects can be eatable:
+// Apple, Carrot, Fire, Fish, Stone
+
+public class SolutionLvl18L7v1 {
+}
+
+class Apple implements Eatable {
+
+}
+
+class Carrot implements Eatable {
+
+}
+
+class Fire {
+
+}
+
+class Fish implements Eatable {
+
+}
+
+class Stone {
+
+}
+
+interface Eatable {
+
+}
