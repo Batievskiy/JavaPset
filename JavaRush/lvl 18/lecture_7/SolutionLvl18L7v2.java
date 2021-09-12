5ebb329413711ef4b80e1db2439bf7f980070930
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-12 11:21:35 +0300

    Add files via upload

---
+++ JavaRush/lvl 18/lecture_7/SolutionLvl18L7v2.java
@@ -0,0 +1,19 @@
+// think of how to inherit three interfaces below:
+// Dance
+// Sing
+// Artist
+
+public class SolutionLvl18L7v2 {
+}
+
+interface Artist extends Dance, Sing {
+
+}
+
+interface Dance {
+
+}
+
+interface Sing {
+
+}
