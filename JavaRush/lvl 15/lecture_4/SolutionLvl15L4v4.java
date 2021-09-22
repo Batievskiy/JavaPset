ed4e6f873b196b2fca1f7a20936f40d583a94bd1
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-22 23:35:35 +0300

    Add files via upload

---
+++ JavaRush/lvl 15/lecture_4/SolutionLvl15L4v4.java
@@ -0,0 +1,17 @@
+// join catch block with similar code into one catch block
+
+public class SolutionLvl15L4v4 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        } catch (IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+    }
+}
