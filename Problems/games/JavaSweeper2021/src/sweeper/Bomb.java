7a9c0d35d6fbf89161e6a2b592bcb2f92640fc24
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-24 15:51:27 +0300

    Add files via upload

--- Problems/games/JavaSweeper2021/src/sweeper/Bomb.java
+++ Problems/games/JavaSweeper2021/src/sweeper/Bomb.java
@@ -6,19 +6,48 @@ class Bomb {
 
     Bomb (int totalBombs) {
         this.totalBombs = totalBombs;
+        fixBombsCount();
     }
 
     void start() {
         bombMap = new Matrix(Box.ZERO);
-        placeBomb();
+        for (int i = 0; i < totalBombs; i++) {
+            placeBomb();
+        }
     }
 
     Box get(Coord coord) {
         return bombMap.get(coord);
     }
 
-    private void placeBomb() {
-        bombMap.set(new Coord(4, 4), Box.BOMB);
+    private void fixBombsCount() {
+        int maxBombs = Ranges.getSize().x * Ranges.getSize().y / 2;
+        if (totalBombs > maxBombs) {
+            totalBombs = maxBombs;
+        }
     }
 
+    private void placeBomb() {
+        while (true) {
+            Coord coord = Ranges.getRandomCord();
+            if (Box.BOMB == bombMap.get(coord)) {
+                continue;
+            }
+            bombMap.set(coord, Box.BOMB);
+            incNumbersAroundBomb(coord);
+            break;
+        }
+    }
+
+    private void incNumbersAroundBomb (Coord coord) {
+        for (Coord around : Ranges.getCoordsAround(coord)) {
+            if (Box.BOMB != bombMap.get(around))
+            bombMap.set(around, bombMap.get(around).getNextNumberBox());
+        }
+    }
+
+    // return total bombs count
+    int getTotalBombs() {
+        return totalBombs;
+    }
 }
