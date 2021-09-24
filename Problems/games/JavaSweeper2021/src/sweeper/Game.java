56b34786393055f38f95c3c27b83a32285151fe2
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-24 15:52:41 +0300

    Add files via upload

--- Problems/games/JavaSweeper2021/src/sweeper/Game.java
+++ Problems/games/JavaSweeper2021/src/sweeper/Game.java
@@ -3,22 +3,116 @@ package sweeper;
 // face class to control the game
 public class Game {
 
+    // first layer with flags (CLOSED, FLAGGED)
+    private Flag flag;
+    // under-layer with bombs and numbers (NUM1...NUM8, BOMB etc.)
     private Bomb bomb;
+    // set game state to know what's going on in the game right now
+    private GameState state;
+
+    public GameState getState() {
+        return state;
+    }
 
     // game constructor uses arguments as columns and rows number
     public Game(int cols, int rows, int bombs) {
         // here we call our setter for sizes using arguments
         Ranges.setSize(new Coord(cols, rows));
         bomb = new Bomb(bombs);
+        flag = new Flag();
     }
 
     public void start() {
         bomb.start();
+        flag.start();
+        state = GameState.PLAYED;
     }
 
     // this method tells what to draw on the screen
     public Box getBox(Coord coord) {
-        return bomb.get(coord);
+        if (flag.get(coord) == Box.OPENED) {
+            return bomb.get(coord);
+        } else {
+            return flag.get(coord);
+        }
     }
 
+    // set opened box into left mouse click coordinates
+    public void pressLeftButton(Coord coord) {
+        if (gameOver()) {
+            return;
+        }
+        openBox(coord);
+        checkWinner();
+    }
+
+    private void checkWinner() {
+        if (state == GameState.PLAYED) {
+            if (flag.getCountOfClosedBoxes() == bomb.getTotalBombs()) {
+                state = GameState.WINNER;
+            }
+        }
+    }
+
+    private void openBox(Coord coord) {
+        switch (flag.get(coord)) {
+            case OPENED : setOpenedToClosedBoxesAroundNumber(coord); return;
+            case FLAGGED: return;
+            case CLOSED :
+                switch (bomb.get(coord)) {
+                    case ZERO : openBoxesAround(coord); return;
+                    case BOMB : openBombs(coord); return;
+                    default : flag.setOpenedToBox(coord);
+                }
+        }
+    }
+
+    private void setOpenedToClosedBoxesAroundNumber(Coord coord) {
+        if (bomb.get(coord) != Box.BOMB) {
+            if (flag.getCountOfFlaggedBoxesAround(coord) == bomb.get(coord).getNumber()) {
+                for (Coord around : Ranges.getCoordsAround(coord)) {
+                    if (flag.get(around) == Box.CLOSED) {
+                        openBox(around);
+                    }
+                }
+            }
+        }
+    }
+
+    private void openBombs(Coord bombed) {
+        state = GameState.BOMBED;
+        flag.setBombedToBox(bombed);
+        for (Coord coord : Ranges.getAllCoords()) {
+            if (bomb.get(coord) == Box.BOMB) {
+                flag.setOpenedToClosedBox(coord);
+            } else {
+                flag.setNoBombToFlaggedSafeBox(coord);
+            }
+        }
+    }
+
+    // open all boxes around clear box
+    private void openBoxesAround(Coord coord) {
+        flag.setOpenedToBox(coord);
+        for (Coord around : Ranges.getCoordsAround(coord)) {
+            // recursive call of openBox()
+            openBox(around);
+        }
+    }
+
+    // set ... into right mouse click coordinates
+    public void pressRightButton(Coord coord) {
+        if (gameOver()) {
+            return;
+        }
+        flag.toggleFlaggedToBox(coord);
+    }
+
+    private boolean gameOver() {
+        if (state == GameState.PLAYED) {
+            return false;
+        }
+        start();
+        return true;
+    }
 }
