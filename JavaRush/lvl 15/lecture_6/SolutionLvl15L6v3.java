9bfb94f1571cb7173c8483e5acd985ac0b589f1d
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-26 19:48:16 +0300

    Add files via upload

---
+++ JavaRush/lvl 15/lecture_6/SolutionLvl15L6v3.java
@@ -0,0 +1,16 @@
+// catch an exception in main and print out stack trace
+
+public class SolutionLvl15L6v3 {
+
+    public static void main(String[] args) {
+        try {
+            dangerousMethod();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void dangerousMethod() throws Exception {
+        throw new Exception("Mu-ha-ha!");
+    }
+}
