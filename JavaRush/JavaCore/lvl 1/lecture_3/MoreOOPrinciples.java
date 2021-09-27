c7b553496f04f668801b014ed0d53cebc34ef97f
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-27 16:48:02 +0300

    Add files via upload

---
+++ JavaRush/JavaCore/lvl 1/lecture_3/MoreOOPrinciples.java
@@ -0,0 +1,47 @@
+public class MoreOOPrinciples {
+    /*
+
+    The Proximity Principle
+    The principle of proximity focuses on how well organized your code
+    is with respect to readability and change.
+    Proximity implies that functions that are changed together
+    are moved closer together.
+    Proximity is both a design principle and a heuristic
+    for refactoring hotspots toward code that's easier to understand.
+
+    The Gestalt law of proximity states:
+    - that "objects or shapes that are close to one another
+    appear to form groups".
+    Even if the shapes, sizes, and objects are radically different,
+    they will appear as a group if they are close.
+
+
+    Common Closure Principle
+    - Classes within a released component should share common closure.
+    That is, if one needs to be changed,
+    they all are likely to need to be changed.
+
+    - Common Closure Principle is the proximity principle—but backward.
+    It's hard to apply and often leads to overcomplicated code.
+    Instead of planning what will change with what,
+    it's better to watch it after some passes and refactor.
+
+
+    The Single-Responsibility Principle
+    - every class in a computer program should have responsibility
+    over a single part of that program's functionality,
+    which it should encapsulate.
+    All of that module, class or function's services
+    should be narrowly aligned with that responsibility.
+
+
+    Chunking
+    Humans learn (and get mastery) thanks to the process called chunking.
+    In cognitive psychology, chunking is a process
+    by which individual pieces of an information set
+    are broken down and then grouped together.
+    A chunk is a collection of basic familiar units
+    that have been grouped together and stored in a person's memory.
+
+     */
+}
