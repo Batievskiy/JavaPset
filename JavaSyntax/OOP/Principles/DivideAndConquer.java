945ed604eeae60d0e62064a02d6daa1d20865f1b
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-27 16:49:37 +0300

    Add files via upload

---
+++ JavaSyntax/OOP/Principles/DivideAndConquer.java
@@ -0,0 +1,22 @@
+public class DivideAndConquer {
+    /*
+
+    Divide and Conquer Principle.
+    There is a division of labor at work.
+    A very complicated and complex transaction
+    has been divided into smaller, more manageable tasks.
+    This divide-and-conquer approach
+    - is an important problem-solving strategy
+    as well as an important element
+    in designing object-oriented programs.
+
+    Divide and Conquer is an algorithmic paradigm.
+    A typical Divide and Conquer algorithm
+    solves a problem using following three steps:
+    1. Divide: Break the given problem
+       into sub-problems of same type.
+    2. Conquer: Recursively solve these sub-problems.
+    3. Combine: Appropriately combine the answers.
+
+     */
+}
---
+++ JavaSyntax/OOP/Principles/OOPrinciples.java
@@ -0,0 +1,60 @@
+public class OOPrinciples {
+
+    /*
+
+    Several characteristics that are important elements
+    of object-oriented programming:
+
+    Divide and Conquer Principle.
+    There is a division of labor at work.
+    A very complicated and complex transaction
+    has been divided into smaller, more manageable tasks.
+    This divide-and-conquer approach is an important
+    problem-solving strategy as well as an important element
+    in designing object-oriented programs.
+
+    Encapsulation Principle.
+    Each agent encapsulates the expertise needed
+    to perform a certain task.
+    Each agent involved in this complicated transaction
+    is an expert in one aspect of the overall process.
+    In addition to knowing how to perform the task,
+    each agent knows exactly what information
+    is required for the task, and exactly what information needs
+    to be passed on to the next agent.
+
+    Interface Principle.
+    In order for agents to work cooperatively and efficiently,
+    we have to clarify exactly how they should interact,
+    or interface, with one another.
+    This is called the interface principle.
+    For example, the interface to the delivery dispatcher
+    in our transaction might be a particular paper
+    form that must be filled in.
+    Once the form is properly filled in,
+    the dispatcher can perform the task.
+
+    Information Hiding Principle.
+    The work that each agent performs is hidden from the other agents.
+    The sales agent knows what information to pass to the warehouse
+    clerk but does not know exactly what the warehouse clerk does
+    with the information.
+
+    Generality Principle.
+    As long as we are designing an object to solve a problem,
+    we might as well do it in a general way
+    so that the object can be used in any situation
+    that requires its expertise. In other words,
+    we design objects not for a particular task,
+    but rather for a particular kind of task.
+
+    Extensibility Principle.
+    One of the strengths of the object-oriented approach
+    is the extensibility of objects.
+    This too has its analogue in the everyday world.
+    Once an agent acquires expertise in a given task,
+    it is not difficult to extend the agent's expertise
+    to accomplish a related task.
+
+     */
+}
