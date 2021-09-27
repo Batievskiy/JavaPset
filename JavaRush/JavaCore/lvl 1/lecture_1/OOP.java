c0f92824fadd431bb13ee9cef2937eb30321b2c4
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-27 12:08:09 +0300

    Add files via upload

---
+++ JavaRush/JavaCore/lvl 1/lecture_1/OOP.java
@@ -0,0 +1,51 @@
+public class OOP {
+    /*
+
+    ABSTRACTION:
+
+    Abstraction is a process where you show only “relevant” data
+    and “hide” unnecessary details of an object from the user.
+
+    ENCAPCULATION:
+
+    Encapsulation is:
+    - Binding the data with the code that manipulates it.
+    - It keeps the data and the code safe from external interference.
+    Encapsulated code should have following characteristics:
+    - Everyone knows how to access it.
+    - Can be easily used regardless of implementation details.
+    - There shouldn’t any side effects of the code, to the rest of the application.
+
+    INHERITANCE:
+
+    Inheritance is the mechanism by which an object acquires
+    the some / all properties of another object.
+    It supports the concept of hierarchical classification.
+
+    POLYMORPHISM:
+
+    Polymorphism means to process objects differently
+    based on their data type.
+    In other words it means, one method with multiple implementation,
+    for a certain class of action. And which implementation
+    to be used is decided at runtime depending upon the situation
+    (i.e., data type of the object).
+    This can be implemented by designing a generic interface,
+    which provides generic methods for a certain class of action
+    and there can be multiple classes, which provides the implementation
+    of these generic methods.
+
+    Polymorphism could be static and dynamic both.
+    Method Overloading is static polymorphism while,
+    Method overriding is dynamic polymorphism.
+    Overloading in simple words means more than one method
+    having the same method name that behaves differently
+    based on the arguments passed while calling the method.
+    This called static because, which method
+    to be invoked is decided at the time of compilation.
+    Overriding means a derived class is implementing a method
+    of its super class. The call to overridden method
+    is resolved at runtime, thus called runtime polymorphism.
+    
+     */
+}
