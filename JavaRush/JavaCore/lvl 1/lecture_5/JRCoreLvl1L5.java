639de66e7e95b9d708490fe837feb7de5d27f3bf
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-29 18:09:10 +0300

    Add files via upload

---
+++ JavaRush/JavaCore/lvl 1/lecture_5/JRCoreLvl1L5.java
@@ -0,0 +1,44 @@
+public class JRCoreLvl1L5 {
+}
+
+class ChessItem {
+    int x;
+    int y;
+    int worth;
+}
+
+class King extends ChessItem {
+    void kingMove() {
+        // some code...
+    }
+}
+
+class Queen extends ChessItem {
+    void queenMove() {
+        // some code...
+    }
+}
+
+ class Rook extends ChessItem {
+    void rookMove() {
+
+    }
+ }
+
+ class Bishop extends ChessItem {
+    void bishopMove() {
+
+    }
+ }
+
+ class Knight extends ChessItem {
+    void knightMove() {
+
+    }
+ }
+
+ class Pawn extends ChessItem {
+    void pawnMove() {
+
+    }
+ }
