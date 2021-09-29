90c12ba8140a35696ee299c530647eb6d17d670e
Batievskiy <batievskiy.e.v@gmail.com>
2021-09-29 18:09:55 +0300

    Add files via upload

---
+++ JavaRush/JavaCore/lvl 1/lecture_7/WhyEncapsulation.java
@@ -0,0 +1,14 @@
+public class WhyEncapsulation {
+    /*
+
+    Why Encapsulation?
+
+    - Better control of class attributes and methods.
+    - Class attributes can be made read-only (if you only use the get method),
+      or write-only (if you only use the set method).
+    - Flexible: the programmer can change one part of the code
+      without affecting other parts.
+    - Increased security of data
+
+     */
+}
