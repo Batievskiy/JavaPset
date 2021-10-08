924063a3780688a23ab8c609754d0e2399281244
Batievskiy <batievskiy.e.v@gmail.com>
2021-10-08 18:31:23 +0300

    Add files via upload

---
+++ JavaSyntax/OOP/Abstraction/AbstractClassVsInterface.java
@@ -0,0 +1,33 @@
+public class AbstractClassVsInterface {
+    /*
+
+    1. Type of method:
+        Interface can have only abstract methods.
+        Abstract class can have abstract and non-abstract methods.
+
+    2. Finale variables:
+        Variables declared in Java interface are by default final!
+        An Abstract class may contain non-final variables.
+
+    3. Type of variables:
+        Interface has only static and final variables.
+        Abstract class can have final, non-final, static and non-static variables.
+
+    4. Implementation:
+        Interface can't provide the implementation of an abstract class.
+        Abstract class can provide the implementation of the interface.
+
+    5. Inheritance vs Abstraction:
+        Interface can be implemented using the key word "implements".
+        Abstract class can be extended using the keyword "extends".
+
+    6. Multiple implementation:
+        Interface can extend another Java interface only!
+        Abstract class can extend another Java class and implement multiple interfaces.
+
+    7. Accessibility of Data Members:
+        Interfaces members are public.
+        Abstract class members can have like private, protected, etc.
+
+     */
+}
