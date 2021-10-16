2d31245a9ecd05385482b59c3d97cd93a3e8e8c0
Batievskiy <batievskiy.e.v@gmail.com>
2021-10-16 20:43:39 +0300

    Add files via upload

--- Problems/nix_8/hw_1_base_operations/src/main/java/BaseOperationsMain.java
+++ Problems/nix_8/hw_1_base_operations/src/main/java/BaseOperationsMain.java
@@ -1,5 +1,9 @@
+package ua.com.alevel;
+
+import java.io.IOException;
+
 public class BaseOperationsMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WantToPlayALittleGame.playALittleGame();
     }
 }
