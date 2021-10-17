a5df3b45b20efe20eceb77d31326964b7a5114f2
Batievskiy <batievskiy.e.v@gmail.com>
2021-10-17 19:20:20 +0300

    Delete GameInterface.java

--- Problems/nix_8/hw_1_base_operations/src/main/java/ua/com/alevel/interfaces/GameInterface.java
+++
@@ -1,8 +0,0 @@
-package ua.com.alevel.interfaces;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-
-public interface GameInterface {
-    void play(BufferedReader bufferedReader) throws IOException;
-}
