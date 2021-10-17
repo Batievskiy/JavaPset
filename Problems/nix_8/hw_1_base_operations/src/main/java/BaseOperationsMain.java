0da87ec1c8670cc32bb6281a637eada614a460e8
Batievskiy <batievskiy.e.v@gmail.com>
2021-10-17 19:20:39 +0300

    Delete BaseOperationsMain.java

--- Problems/nix_8/hw_1_base_operations/src/main/java/BaseOperationsMain.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel;
-
-import java.io.IOException;
-
-public class BaseOperationsMain {
-    public static void main(String[] args) throws IOException {
-        WantToPlayALittleGame.playALittleGame();
-    }
-}
