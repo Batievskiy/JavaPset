5005bf4b726f303f1b9a14d1293c8132aa65a035
Batievskiy <batievskiy.e.v@gmail.com>
2021-10-17 19:19:24 +0300

    Add files via upload

---
+++ Problems/nix_8/hw_1_base_operations/src/main/java/ua/com/alevel/interfaces/Game.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.interfaces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface Game {
+    void play(BufferedReader bufferedReader) throws IOException;
+}
